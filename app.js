0dc8d2c9da072df0dc21db41285c0834e01fc987
Siddhartha <chatterjeesid1993@gmail.com>
2018-07-14 12:16:33 +0530

    Seeded the database and added Comment Model

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    Campground = require('./models/campgrounds'),
+    Comment = require('./models/comments'),
+    seedDB = require('./seeds');
 
 // Connecting MONGOOSE
 mongoose.connect('mongodb://localhost/yelp_camp');
-
-// Schema
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-// Modelling
-var Campground = mongoose.model('Campground',campgroundSchema);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -24,44 +17,7 @@ app.get('/',function(req,res){
     console.log('Homepage requested');
 });
 
-
-// Campground.create(
-//     [
-//         {
-//             name : "Creek Row", 
-//             image : "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-//             description: "This is in the midst of Peru. Temperature is nominal and water is crystal clear. No signal reception."
-//         },
-//         {
-//             name : "Granite Hill", 
-//             image : "https://images.pexels.com/photos/730426/pexels-photo-730426.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-//             description: "This is a huge granite hill. No bathrooms. No water. Just beautiful granite."
-//         },
-//         {
-//             name : "Coleman Ridge",
-//             image : "https://images.pexels.com/photos/618848/pexels-photo-618848.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-//             description: "Greenery amongst snow white snow. A combination of Greenland and Iceland, except you know which is which."
-//         },
-//         {
-//             name : "Canyon Abyss", 
-//             image : "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-//             description: "It's like home. Except it is not. Experience the comfort of home and nothing more after you've paid a hefty sum to us. Very practical."
-//         },    
-//         {
-//             name : "Leviathan Heights", 
-//             image : "https://images.pexels.com/photos/965153/pexels-photo-965153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-//             description: "The name is ironical since it is the opposite of height. It's the deepest trench in America, and oh, it doesn't exist in case you were wondering"
-//         }
-//     ],
-//     function(err,campground){
-//         if(err)
-//         console.log(err);
-//         else{
-//             console.log(campground);
-//         }
-//     }
-// )
-
+seedDB();
 
 // Index Route to view all campground lists
 app.get('/campgrounds',function(req,res){
@@ -70,7 +26,7 @@ app.get('/campgrounds',function(req,res){
             console.log('There was an error');
         }
         else{
-            res.render('index',{campgrounds:campgrounds});
+            res.render('campgrounds/index',{campgrounds:campgrounds});
         }
     })
     console.log('Index page requested');
@@ -78,12 +34,7 @@ app.get('/campgrounds',function(req,res){
 
 // Create Route to add new campgrounds to the database
 app.post('/campgrounds',function(req,res){
-    var newgroundname = {
-        name:req.body.name,
-        image:req.body.image,
-        description:req.body.description
-    };
-    Campground.create(newgroundname, function(err,campground){
+    Campground.create(req.body.newground, function(err,campground){
         if(err){
             console.log('There was an error');
         }
@@ -95,22 +46,38 @@ app.post('/campgrounds',function(req,res){
 
 // New Route to add new campgrounds to the form
 app.get('/campgrounds/newground',function(req,res){
-    res.render('newground');
+    res.render('campgrounds/newground');
     console.log('New campground form requested');
 });
 
 // Show Route to show more info on a single campground
 app.get('/campgrounds/:id', function(req,res){
-    Campground.findById(req.params.id, function(err,foundCampground){
+    Campground.findById(req.params.id).populate('comments').exec(function(err,foundCampground){
         if(err){
             console.log(err);
         }
         else{
-            res.render('show',{campground:foundCampground});
+            res.render('campgrounds/show',{campground:foundCampground});
         }
     });
     console.log(`Requested ${req.params.id} page`);
-})
+});
+
+// app.get('/campgrounds/:id/comments/new',function(req,res){
+//     Campground.findById(req.params.id,function(err,foundCampground){
+//         if(err)
+//         console.log(err);
+//         res.render('new',{campground:foundCampground});
+//     });
+// });
+
+// app.post('campgrounds/:id',function(req,res){
+//     Campground.create(req.body.comment,function(err,comment){
+//         if(err)
+//         console.log(err);
+
+//     })
+// })
 
 app.listen(3000,function(){
     console.log('YelpCamp server has started');
