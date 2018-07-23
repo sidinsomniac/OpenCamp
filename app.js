33b89b456d9edfaf3703307fb07be17a295369f1
Siddhartha <chatterjeesid1993@gmail.com>
2018-07-23 20:13:28 +0530

    Associated Comments to the User

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+'/public'));
-seedDB();
+// seedDB();
 
 // Passport Configuration
 app.use(expressSession({
