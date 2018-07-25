5ce283ce8caae5aa69de8d7c898350ef7929c645
Siddhartha <chatterjeesid1993@gmail.com>
2018-07-25 21:22:07 +0530

    Added comment authorization for editing and deleting

--- routes/comments.js
+++ routes/comments.js
@@ -38,7 +38,7 @@ router.post('/',isLoggedIn,function(req,res){
     });
 });
 
-router.get('/:comment_id/edit',function(req,res){
+router.get('/:comment_id/edit',commentAuthorize,function(req,res){
     Comment.findById(req.params.comment_id,function(err,foundComment){
         if(err)
         res.redirect('back');
@@ -48,7 +48,7 @@ router.get('/:comment_id/edit',function(req,res){
     });
 });
 
-router.put('/:comment_id',function(req,res){
+router.put('/:comment_id',commentAuthorize,function(req,res){
     Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updateComment){
         if(err)
         res.redirect('back');
@@ -59,7 +59,7 @@ router.put('/:comment_id',function(req,res){
     });
 });
 
-router.delete('/:comment_id',function(req,res){
+router.delete('/:comment_id',commentAuthorize,function(req,res){
     Comment.findByIdAndRemove(req.params.comment_id,function(err){
         if(err)
         res.redirect('back');
@@ -76,5 +76,28 @@ function isLoggedIn(req,res,next){
     }
     res.redirect('/login');
 }
+
+// Authorize campground function
+function commentAuthorize(req,res,next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id,function(err,foundComment){
+            if(err)
+            res.redirect('back');
+            else{
+                if(foundComment.author.id.equals(req.user.id)){
+                    next();                    
+                }
+                else{
+                    res.redirect('back');
+                }
+            }
+        });
+    }
+    else{
+        res.redirect('back');
+    }
+}
+
+
 
 module.exports = router;
