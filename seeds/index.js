8c5a727cae36249cbf8c0242b4f5c59118f641f3
sidinsomniac <chatterjeesid1993@gmail.com>
2021-06-12 19:02:46 +0530

    Upgrade seed descriptors

--- seeds/index.js
+++ seeds/index.js
@@ -17,23 +17,30 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+const authors = ["613b372fdc48d645fc153011", "613b41d32a6b1713f4aca84e", "613b5d0beb1766193048fb54"];
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const randomize = (sampleLength) => {
+    return Math.floor(Math.random() * sampleLength);
+};
 
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+    for (let i = 0; i < 10; i++) {
         const price = Math.floor(Math.random() * 20) + 10;
+        const campgroundCity = cities[randomize(406)];
+        const author = authors[randomize(3)];
         const camp = new Campground({
-            author: '613b372fdc48d645fc153011',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            author,
+            location: `${campgroundCity.city}, ${campgroundCity.state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
+                coordinates: [campgroundCity.longitude, campgroundCity.latitude]
             }
         });
         await camp.save();
